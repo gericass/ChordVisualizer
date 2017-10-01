310854527d86269d7294080888073a9b23716133
Keita <bonjovi1926@yahoo.co.jp>
2017-10-01 20:23:36 +0900

    add: mysql driver

--- src/main/kotlin/db/DB.kt
+++ src/main/kotlin/db/DB.kt
@@ -1,2 +1,12 @@
 package db
 
+import org.jetbrains.exposed.sql.*
+
+class DB {
+
+
+    init {
+        Database.connect("jdbc:mysql://127.0.0.1:13306/chord_dht_repo", "com.mysql.jdbc.Driver","root","mysql")
+    }
+
+}
